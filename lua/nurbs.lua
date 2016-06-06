1ac4efd0d4f4fdf2c9d91aeaa6f773f5e7d7b2f3
Reed Weichler <rweichler@gmail.com>
2016-06-05 21:53:15 -0700

    moved code over, it segfaults

--- lua/nurbs.lua
+++ lua/nurbs.lua
@@ -5,61 +5,43 @@ local NURBS = GRAPH()
 function NURBS:new()
     local self = GRAPH.new(self)
 
-    self.spline = ffi.new("tsBSpline[1]")
-
     return self
 end
 
 local DIM = 4
-function NURBS:init(dont_free)
+function NURBS:init()
     local points = self.points
     local knots = self.knots
     assert(self.degree)
     assert(points)
     assert(knots)
     assert(#points + self.degree + 1 == #knots)
-    local TS_CLAMPED = 1
-    if not dont_free then
-        ffi.C.ts_bspline_free(self.spline)
-    end
-    local err = ffi.C.ts_bspline_new(self.degree, DIM, #points, TS_CLAMPED, self.spline)
-    if err < 0 then
-        error(ffi.C.ts_enum_str(err))
+
+
+    if self.c_shit then
+        BSPLINE_FREE(self.c_shit[1], self.c_shit[2])
     end
 
-    self:update_points()
-    self:update_knots()
+    self.c_shit = {BSPLINE_SETUP(#points, #knots, DIM, points, knots)}
 end
 
-function NURBS:update_points()
-    local points = self.points
-    local spline = self.spline[0]
-    for i,v in ipairs(points) do
-        local idx = (i - 1)*DIM
-        for i=1,DIM do
-            spline.ctrlp[idx + i - 1] = v[i]
-        end
-    end
-end
-function NURBS:update_knots()
-    local knots = self.knots
-    local spline = self.spline[0]
-    for i, v in ipairs(knots) do
-        spline.knots[i - 1] = v
-    end
-end
+NURBS.update_points = NURBS.init
+NURBS.update_knots = NURBS.init
 
 function NURBS:coords()
-    if not self.points and not self.knots and not self.spline then return end
+    if not self.points and not self.knots and not self.c_shit then return end
     local first = self.knots[self.degree + 1]
     local last = self.knots[#self.knots - self.degree]
     local span = last - first
     local EVALUATE_SPLINE = EVALUATE_SPLINE
     local spline = self.spline
+
+    local points = self.c_shit[1]
+    local knots = self.c_shit[2]
     return function(t)
         local u = t*span + first
-        local pt = EVALUATE_SPLINE(spline, u)
-        return pt[0]/pt[3], pt[1]/pt[3], pt[2]/pt[3]
+        local x, y, z, w = BSPLINE_EVAL(points, knots, #self.points, #self.knots, DIM, u, self.degree)
+        return x/w, y/w, z/w
     end
 end
 
